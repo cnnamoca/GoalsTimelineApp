a5fb409adfb0ae0d428fc5b3058e9409a20f9db5
Mar Koss <kossmar2@gmail.com>
2017-10-30 14:45:12 -0700

    created NSManagedObjectClass for timeline and steppingStones

---
+++ GoalsTimelineApp/SteppingStone+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  SteppingStone+CoreDataClass.swift
+//  GoalsTimelineApp
+//
+//  Created by Mar Koss on 2017-10-30.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(SteppingStone)
+public class SteppingStone: NSManagedObject {
+
+}
