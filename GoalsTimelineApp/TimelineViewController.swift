45a7cd4a5223277ba73515a33b0569a6292ff03f
Carlo <carlo.namoca@gmail.com>
2017-10-30 17:10:05 -0700

    create timeline viewcontroller and cells

---
+++ GoalsTimelineApp/TimelineViewController.swift
@@ -0,0 +1,79 @@
+//
+//  TimelineViewController.swift
+//  GoalsTimelineApp
+//
+//  Created by Carlo Namoca on 2017-10-30.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongGesture(gesture:)))
+        self.collectionView.addGestureRecognizer(longPressGesture)
+        
+    }
+    
+    @objc
+    func handleLongGesture(gesture: UILongPressGestureRecognizer){
+        switch (gesture.state){
+        case UIGestureRecognizerState.began:
+            guard
+                let indexPath = self.collectionView.indexPathForItem(at: gesture.location(in: self.collectionView))
+                else { break }
+            
+            let began = collectionView.beginInteractiveMovementForItem(at: indexPath)
+            print("began \(indexPath): \(began)")
+            break
+            
+        case UIGestureRecognizerState.changed:
+            print("changed")
+            collectionView.updateInteractiveMovementTargetPosition(gesture.location(in: gesture.view!))
+            break
+            
+        case UIGestureRecognizerState.ended:
+            print("ended")
+            //update cell name
+            //update cell below/above as well
+            
+            self.collectionView.reloadData()
+            collectionView.endInteractiveMovement()
+            break
+            
+        default:
+            print("default")
+            collectionView.cancelInteractiveMovement()
+            break
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: "timelineCell", for: indexPath) as! TimelineCollectionViewCell
+        cell.titleLabel.text = "\(indexPath.row)"
+        
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        //needs to be updated
+        return 5
+    }
+    
+    //    func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
+    //        print("can move")
+    //        return true
+    //    }
+    
+    func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        print("move item \(sourceIndexPath) to \(destinationIndexPath)")
+        //update datasource
+    }
+}
