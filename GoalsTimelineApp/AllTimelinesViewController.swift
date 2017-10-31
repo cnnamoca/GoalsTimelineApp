ab354389bf0da5500b6c58f13ccef9b27ad41de6
Mar Koss <kossmar2@gmail.com>
2017-10-31 12:37:43 -0700

    Halfway done updating AllTimelineView to display timeline data

--- GoalsTimelineApp/AllTimelinesViewController.swift
+++ GoalsTimelineApp/AllTimelinesViewController.swift
@@ -10,13 +10,6 @@ import UIKit
 import CoreData
 
 class AllTimelinesViewController: UIViewController, NSFetchedResultsControllerDelegate, UITableViewDelegate, UITableViewDataSource  {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
     
     
     var managedObjectContext : NSManagedObjectContext? = nil
@@ -24,12 +17,26 @@ class AllTimelinesViewController: UIViewController, NSFetchedResultsControllerDe
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         self.fetchTimelineData()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return timelineArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "timelineCell" , for: indexPath)
+        let timeline : Timeline = self.timelineArray[indexPath.row]
+        cell.textLabel?.text = timeline.title
+//        cell.detailTextLabel?.text =
+        
+        return cell
+    
     }
     
     func fetchTimelineData() {
