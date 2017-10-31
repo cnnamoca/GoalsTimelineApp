eeba63bbb087fe7faa1f0066766b0d08b0981e61
olga-nest <olga.w.nesterova@gmail.com>
2017-10-30 18:30:22 -0700

    Make NewTimeline Note multiline

--- GoalsTimelineApp/NewTimelineViewController.swift
+++ GoalsTimelineApp/NewTimelineViewController.swift
@@ -10,14 +10,15 @@ import UIKit
 
 class NewTimelineViewController: UIViewController {
     @IBOutlet weak var newTimelineTitleView: UITextField!
-    @IBOutlet weak var newTimelineNotesView: UITextField!
+    
+    @IBOutlet weak var newTimelineNotesView: UITextView!
     @IBOutlet weak var startDatePicker: UIDatePicker!
     @IBOutlet weak var endDatePicker: UIDatePicker!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setupTextfieldsView()
+     //   self.setupTextfieldsView()
 
         // Do any additional setup after loading the view.
     }
@@ -32,14 +33,14 @@ class NewTimelineViewController: UIViewController {
     }
 
     //Changes textfields borders color
-    func setupTextfieldsView() {
-    let myColor : UIColor = UIColor.gray
-        newTimelineTitleView.layer.borderColor = myColor.cgColor
-        newTimelineNotesView.layer.borderColor = myColor.cgColor
-        
-        newTimelineTitleView.layer.borderWidth = 1
-        newTimelineNotesView.layer.borderWidth = 1
-    }
+//    func setupTextfieldsView() {
+//    let myColor : UIColor = UIColor.gray
+//        newTimelineTitleView.layer.borderColor = myColor.cgColor
+//
+//
+//        newTimelineTitleView.layer.borderWidth = 1
+//
+//    }
     /*
     // MARK: - Navigation
 
