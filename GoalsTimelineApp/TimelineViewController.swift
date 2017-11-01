d6f24356316bd0ccc991a10991e72086de804f66
olga-nest <olga.w.nesterova@gmail.com>
2017-10-31 21:07:33 -0700

    Pass steppingStone object to edit view

--- GoalsTimelineApp/TimelineViewController.swift
+++ GoalsTimelineApp/TimelineViewController.swift
@@ -46,26 +46,23 @@ class TimelineViewController: UIViewController, UICollectionViewDataSource, UICo
         let addSteppingStoneVC : AddSteppingStoneViewController = segue.destination as! AddSteppingStoneViewController
         addSteppingStoneVC.timelineObject = timeline
         }
-//        if segue.identifier == "toEditSteppingStone" {
-//            let editSteppingStoneVC: EditSteppingViewController = segue.destination as! EditSteppingViewController
-//
-//            var steppingArray : Array<SteppingStone> = (timeline.steppingStones)?.allObjects as! Array<SteppingStone>
-////
-//////            let indexPath = self.collectionView.indexPath
-//           var steppingStone : SteppingStone = steppingArray[indexPath.row]
-//            editSteppingStoneVC.steppingStoneObject = steppingStone
-//        }
+        if segue.identifier == "toEditSteppingStone" {
+            let editSteppingStoneVC: EditSteppingViewController = segue.destination as! EditSteppingViewController
+            
+            let steppingStone : SteppingStone = sender as! SteppingStone
+            editSteppingStoneVC.steppingStoneObject = steppingStone
+        }
 
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        //check if steppingstone is not empty
-        //performsegue
-        performSegue(withIdentifier: "toEditSteppingStone", sender: self)
-   //     let editSteppingStoneVC: EditSteppingViewController = segue.destination as! EditSteppingViewController
+        //TODO: check if steppingstone is not empty
+
         var steppingArray : Array<SteppingStone> = (timeline.steppingStones)?.allObjects as! Array<SteppingStone>
-        let steppingStone : SteppingStone = steppingArray[indexPath.row]
-     //   editSteppingStoneVC.steppingStoneObject = steppingStone
+        let selectedItem: SteppingStone = steppingArray[indexPath.item]
+        let steppingStone : SteppingStone = selectedItem
+        
+        performSegue(withIdentifier: "toEditSteppingStone", sender: steppingStone)
         
     }
     
