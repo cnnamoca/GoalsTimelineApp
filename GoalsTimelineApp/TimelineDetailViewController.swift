0cff7d4970ddac4c991a80cd1da1da8064e831c1
olga-nest <olga.w.nesterova@gmail.com>
2017-11-02 12:08:37 -0700

    Add TimelineDetailView, connect all outlets and actions

---
+++ GoalsTimelineApp/TimelineDetailViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class TimelineDetailViewController: UIViewController {
+
+    @IBOutlet weak var editTimelineTitleTextField: UITextField!
+    @IBOutlet weak var editTimelineNotesTextView: UITextView!
+    @IBOutlet weak var timelineStartDatePicker: UIDatePicker!
+    @IBOutlet weak var timelineEndDatePicker: UIDatePicker!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    @IBAction func cancelTimelineEditing(_ sender: UIBarButtonItem) {
+        @IBAction func saveTimelineChanges(_ sender: UIBarButtonItem) {
+        }
+    }
+}
