0043b922f6b251c2abb30f74c2d1ab1110d9cf28
olga-nest <olga.w.nesterova@gmail.com>
2017-11-02 13:02:19 -0700

    Update timeline info

--- GoalsTimelineApp/TimelineDetailViewController.swift
+++ GoalsTimelineApp/TimelineDetailViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import CoreData
 
 class TimelineDetailViewController: UIViewController {
 
@@ -6,19 +7,38 @@ class TimelineDetailViewController: UIViewController {
     @IBOutlet weak var editTimelineNotesTextView: UITextView!
     @IBOutlet weak var timelineStartDatePicker: UIDatePicker!
     @IBOutlet weak var timelineEndDatePicker: UIDatePicker!
-    
+    var timelineObject : Timeline = Timeline ()
+
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.populateFieldsWithTimelineData()
         
 
     }
     @IBAction func cancelTimelineEditing(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func saveTimelineChanges(_ sender: UIBarButtonItem) {
-         self.dismiss(animated: true, completion: nil)
+        let appDelegate : AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        let _ : NSPersistentContainer = appDelegate.persistentContainer
+                
+        self.timelineObject.title = editTimelineTitleTextField.text
+        self.timelineObject.info = editTimelineNotesTextView.text
+        self.timelineObject.startDate = timelineStartDatePicker.date as NSDate
+        self.timelineObject.endDate = timelineEndDatePicker.date as NSDate
+        
+        appDelegate.saveContext()
+        self.dismiss(animated: true, completion: nil)
         }
+    
+    func populateFieldsWithTimelineData() {
+        editTimelineTitleTextField.text = timelineObject.title
+        editTimelineNotesTextView.text = timelineObject.info
+        timelineStartDatePicker.date = timelineObject.startDate! as Date
+        timelineEndDatePicker.date = timelineObject.endDate! as Date
+    }
     
 }
